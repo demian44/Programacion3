c0f7e0a18bd9bc93468934d28ac659feb9b1c8d0
demian44 <demi-44@hotmail.com>
2018-05-29 20:39:11 -0300

    ..

---
+++ Api/index.php
@@ -0,0 +1,38 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require './vendor/autoload.php';
+
+$app = new \Slim\App;
+// $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+//     $name = $args['name'];
+//     $response->getBody()->write("Hello, $name");
+
+//     return $response;
+// });
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+     $name = $args['name'];
+     $response->getBody()->write("Hello $name");
+    //$response->getBody()->write("Hello world");
+    return $response;
+});
+
+$app->post('/hello', function (Request $request, Response $response, array $args) {
+    //$name =  $request["name"];
+    $parsedBody = $request->getParsedBody();
+    $name = $parsedBody['name'];
+    //$name = $args['name'];
+    $response->getBody()->write("Hello $name");
+    return $response;
+});
+
+$app->post('/chao', function (Request $request, Response $response, array $args) {
+    //$name =  $request["name"];    
+    $response->getBody()->write("Hello $name");
+    return $response;
+});
+
+$app->run();
+?>
